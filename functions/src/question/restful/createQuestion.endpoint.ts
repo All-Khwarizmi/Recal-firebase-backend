9d8267fc0f9f9f40fc52930f7320d6c5690df514
Jason Suarez <swarecito@gmail.com>
2023-05-21 23:07:41 +0200

    📦 NEW: Add onQuestionCreated reactive function

--- functions/src/question/restful/createQuestion.endpoint.ts
+++ functions/src/question/restful/createQuestion.endpoint.ts
@@ -5,13 +5,18 @@ import { Post } from 'firebase-backend';
 const db = admin.firestore();
 
 export default new Post(async (request: Request, response: Response) => {
-  const { quizzName, question, correctAnswer, answers } = request.body;
+  const { quizzName, question, correctAnswer, answers, classId, id } =
+    request.body;
 
   const questionCreation = await db
     .collection('quizz')
     .doc(quizzName)
     .collection('questions')
-    .add({ question, correctAnswer, answers, quizzName });
+    .doc(id)
+    .set(
+      { question, correctAnswer, answers, quizzName, classId },
+      { merge: true }
+    );
 
   response.status(201).send({ questionCreation });
 });
---
+++ functions/src/quizz/reactive/onQuizzUpdated.function.ts
@@ -0,0 +1,47 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+const db = admin.firestore();
+const logInfo = functions.logger.info;
+
+export default functions.firestore
+  .document('quizz/{quizzId}/questions/{questionId}')
+  .onCreate(async (questionSnapshot, context) => {
+    logInfo(
+      `questionSnapshot: ${
+        questionSnapshot.data()['question']
+      } from the quizz ${context.params.quizzId}, ${context.params.questionId}`
+    );
+    // Get users ref
+    const usersRef = db.collection('users');
+
+    // Get users from the concerned class
+    const usersSnapshot = await usersRef
+      .where('classId', '==', questionSnapshot.data()['classId'])
+      .get();
+
+    // Check if no users
+    if (usersSnapshot.empty) {
+      logInfo('No matching documents.');
+      return;
+    }
+
+    // Todo: add logic
+    usersSnapshot.forEach(async (doc) => {
+      logInfo(
+        `${doc.data()['classId']}, ${doc.data()['name']}, ${
+          doc.data()['notificationTokenId']
+        }`
+      );
+
+    // Add question to quizz on user sc 
+      await db
+        .collection('users')
+        .doc(doc.data()['id'])
+        .collection('todoQuizz')
+        .doc(context.params.quizzId)
+        .collection('questions')
+        .doc(context.params.questionId)
+        .set(questionSnapshot.data(), { merge: true });
+    });
+  });
